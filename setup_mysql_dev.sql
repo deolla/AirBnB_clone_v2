f8d04679a18ece1e153c0b3d572ee51188ec7074
Joshua Emore <ehimareemore@gmail.com>
2023-09-17 19:55:52 +0100

    MYSQL script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,9 +3,10 @@
 -- This creates the database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- This creates a new user if it does not exist
-CREATE USER IF NOT EXISTS 'hbnd_dev'@'localhost' IDENTIFIED BY 'hbnd_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- This line grants all privileges to the database
-GRANT ALL PRIVILEGES ON hbnd_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
 -- This line grants SELECT privilege to the database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
