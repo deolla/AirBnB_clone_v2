874f2be3a859e7492029732e19e2ab6e48963ffc
Joshua Emore <ehimareemore@gmail.com>
2023-09-18 19:40:37 +0100

    Changes to user.py

--- models/user.py
+++ models/user.py
@@ -4,12 +4,13 @@
 from models.base_model import BaseModel, Base
 from models import storage_type
 from sqlalchemy import Column, String
+from os import getenv
 """from sqlalchemy.orm import relationship"""
 """from sqlalchemy.ext.declarative import declarative_base"""
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if storage_type == "db":
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
