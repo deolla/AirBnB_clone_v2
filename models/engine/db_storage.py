6588e278532eca14ec8ce08d846c1b23cf9ccd2e
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-09-17 20:45:30 -0700

    changes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""db storage"""
-from os import getenv
-from sqlalchemy import create_engine, MetaData
+"""This is a db storage module"""
+import os
+from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from models.base_model import Base
 from models.user import User
@@ -11,9 +11,6 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
-metadata_obj = MetaData()
-
-
 class DBStorage:
     """Private class attributes for db storage"""
     __engine = None
@@ -22,17 +19,17 @@ class DBStorage:
     classes = [User, State, Place, City, Amenity, Review]
 
     def __init__(self):
-        user = getenv("HBNB_MYSQL_PWD")
-        passwd = getenv("HBNB_MYSQL_PWD")
-        host = getenv("HBNB_MYSQL_HOST")
-        db = getenv("HBNB_MYSQL_DB")
+        user = os.getenv("HBNB_MYSQL_USER")
+        passwd = os.getenv("HBNB_MYSQL_PWD")
+        host = os.getenv("HBNB_MYSQL_HOST")
+        db = os.getenv("HBNB_MYSQL_DB")
 
         self.__engine = create_engine(
             'mysql+mysqldb://{}:{}@{}/{}'.format(
                 user, passwd, host, db), pool_pre_ping=True)
 
-        if getenv("HBNB_ENV") == "test":
-            Base.metadata.dropall(bind=self.__engine)
+        if os.getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """Query objects from the database based on class name (cls)"""
@@ -54,19 +51,26 @@ class DBStorage:
 
     def new(self, obj):
         """Add the object to the current database session"""
-        self.__session.add(obj)
+        session = self.__session()
+        session.add(obj)
 
     def save(self):
         """commit all changes of the current database session"""
-        self.__session.commit()
+        session = self.__session()
+        session.commit()
 
     def delete(self, obj=None):
         """delete from the current database session"""
-        if obj:
-            self.session.delete(obj)
+        if obj is not None and type(obj) in self.classes:
+            session = self.__session()
+            session.query(type(obj)).filter(type(obj).id == obj,id.delete()
 
     def reload(self):
         """create all tables in the database"""
         Base.metadata.create_all(self.__engine)
         session_pop = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session_pop)
+
+    def close(self):
+        """This is a close method"""
+        self.__session.remove()
