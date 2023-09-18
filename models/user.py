0d55b646aa7ac1d58dbf53448e5523b60bc48c8b
Joshua Emore <ehimareemore@gmail.com>
2023-09-18 21:14:07 +0100

    Changes to user.py

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
