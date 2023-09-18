dc0d561939aac8b5047026705a84aa59c7450773
Joshua Emore <ehimareemore@gmail.com>
2023-09-18 19:41:54 +0100

    Changes to user.py

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This module defines a class User"""
 
 from models.base_model import BaseModel, Base
-from models import storage_type
+"""from models import storage_type"""
 from sqlalchemy import Column, String
 from os import getenv
 """from sqlalchemy.orm import relationship"""
