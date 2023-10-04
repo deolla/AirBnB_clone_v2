3d15c1c3138fa3cc54ea629fe315586b9f65d2c6
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-04 10:24:25 -0700

    changes

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,19 +10,13 @@ from os.path import exists
 
 def do_pack():
     """Generates a .tgz archive from the web_static folder"""
-
-    # Create the versions folder if it doesn't exist
     local('mkdir -p versions')
-
-    # Generate the archive path
     current_time = datetime.now().strftime('%Y%m%d%H%M%S')
     archive_path = 'versions/web_static_{}.tgz'.format(current_time)
 
     try:
-        # Create the .tgz archive from the web_static folder
         local('tar -czvf {} web_static'.format(archive_path))
 
-        # Check if the archive has been generated
         if exists(archive_path):
             return archive_path
 
