ae119dc8f674f11e4455b83b7f6db8182c6c4ca2
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-04 11:00:48 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,12 +24,14 @@ def do_deploy(archive_path):
         folder_name = filename.split('.')[0]
 
         run('mkdir -p /data/web_static/releases/{}'.format(folder_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, folder_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+            filename, folder_name))
 
         run('rm /tmp/{}'.format(filename))
         run('rm -rf /data/web_static/current')
 
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder_name))
+        run('ln -s /data/web_static/releases/{}/ '
+            '/data/web_static/current'.format(folder_name))
 
         return True
 
