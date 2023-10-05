b0cd8ac9749b035e2bb44d144c1e2b7a98f20a05
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-05 13:04:07 -0700

    changes

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Fabric script"""
+
 from fabric.api import local
 from time import strftime
 from datetime import date
@@ -7,12 +8,11 @@ from datetime import date
 
 def do_pack():
     """A Generates archive the contents of web_static folder"""
-
     f = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(f))
+                .format(f))
 
         return "versions/web_static_{}.tgz".format(f)
 
