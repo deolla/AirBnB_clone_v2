5652ba58e46ea54cef0f86e6e2bb5fa9e67dbeb5
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-21 10:41:33 -0700

    changes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" module doc """
+"""Starts a Flask web application """
 from flask import Flask
 from flask import render_template
 from models import storage
@@ -10,38 +10,54 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
-    """ def doc """
+    """Routes:
+            /: display Hello HBNB!
+            /hbnb: display HBNB
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """ def doc """
+    """Routes:
+            /: display Hello HBNB!
+            /hbnb: display HBNB
+    """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """ def doc """
-    return 'c {}'.format(text.replace("_", " "))
+def c_text(text):
+    """c/<text>: display followed by the value of text
+            (replace underscore _ symbols with a space
+    """
+    return 'C {}'.format(text.replace("_", " "))
 
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
-    """ def doc """
+    """ Display Python, followed by the value of the text variable
+            (replace underscore _ symbols with a space)
+    """
     return 'Python {}'.format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """ def doc """
+def is_number(n):
+    """/number/<n>: display is a number only if n is an integer"""
     return '{} is a number'.format(n)
 
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def is_number_html(n):
+    """/number_template/<n>: display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """ def doc """
+def odd_or_even(n):
+    """/number_odd_or_even/<n>: display a HTML page only if n is integer"""
     if n % 2 == 0:
         p = 'even'
     else:
