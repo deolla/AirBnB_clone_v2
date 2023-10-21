1d3a92d07d1f22893658d3e27b0b612be81a0aea
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-21 13:50:28 -0700

    changes

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that starts a Flask web application """
+"""Starts a Flask web application"""
 from flask import Flask
 from flask import render_template
 from models import storage
